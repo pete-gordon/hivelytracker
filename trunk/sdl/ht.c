77a29822e0e80f6b38c18c0af43e269775328672
chris.oneill@gmail.com <chris.oneill@gmail.com@247bc888-1e01-a638-0bbc-bca61d7182f5>
2012-10-19 10:51:12 +0000

    OSX: Disable fullscreen and workaround for reveresed byte order screen format bug in SDL

--- trunk/sdl/ht.c
+++ trunk/sdl/ht.c
@@ -38,11 +38,11 @@ BOOL init( void )
     srfdepth = info->vfmt->BitsPerPixel;
 
   // Try to setup the video display
-#ifndef WIN32
-  ssrf = SDL_SetVideoMode( 800, 600, srfdepth, pref_fullscr ? SDL_FULLSCREEN : SRFTYPE );
-#else
-  // requesters cause all kinds of problems for fullscreen on windows, so ignore it
+#if defined(WIN32) || defined(__APPLE__)
+  // requesters cause all kinds of problems for fullscreen on windows and OSX, so ignore it
   ssrf = SDL_SetVideoMode( 800, 600, srfdepth, SRFTYPE );
+#else
+  ssrf = SDL_SetVideoMode( 800, 600, srfdepth, pref_fullscr ? SDL_FULLSCREEN : SRFTYPE );
 #endif
   if( !ssrf )
   {
