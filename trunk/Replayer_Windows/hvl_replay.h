4e6734d4d3eca845e8d3128c63cc8c704ccde04e
fredrikahlstrom75@gmail.com <fredrikahlstrom75@gmail.com@247bc888-1e01-a638-0bbc-bca61d7182f5>
2014-12-17 09:41:10 +0000

    Added separate hvl_ParseTune function so one can send in a pointer to an already loaded tune and get it parsed.

--- trunk/Replayer_Windows/hvl_replay.h
+++ trunk/Replayer_Windows/hvl_replay.h
@@ -229,5 +229,6 @@ void hvl_DecodeFrame( struct hvl_tune *ht, int8 *buf1, int8 *buf2, int32 bufmod 
 void hvl_InitReplayer( void );
 BOOL hvl_InitSubsong( struct hvl_tune *ht, uint32 nr );
 struct hvl_tune *hvl_LoadTune( const TEXT *name, uint32 freq, uint32 defstereo );
+struct hvl_tune *hvl_ParseTune(const uint8 *buf, uint32 buflen, uint32 freq, uint32 defstereo );
 void hvl_FreeTune( struct hvl_tune *ht );
 
