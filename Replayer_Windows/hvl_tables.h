921cf0dc765dc0438425721d5128361fce519352
Stian Skjelstad <stian.skjelstad@gmail.com>
2020-02-28 23:24:05 +0100

    Uniform hvl_tables.h to one common source

--- Replayer_Windows/hvl_tables.h
+++ Replayer_Windows/hvl_tables.h
@@ -1,31 +1,6 @@
-#define WHITENOISELEN (0x280*3)
+#ifndef _REPLAYER_WINDOWS_HVL_TABLES_H
+#define _REPLAYER_WINDOWS_HVL_TABLES_H
 
-#define WO_LOWPASSES   0
-#define WO_TRIANGLE_04 (WO_LOWPASSES+((0xfc+0xfc+0x80*0x1f+0x80+3*0x280)*31))
-#define WO_TRIANGLE_08 (WO_TRIANGLE_04+0x04)
-#define WO_TRIANGLE_10 (WO_TRIANGLE_08+0x08)
-#define WO_TRIANGLE_20 (WO_TRIANGLE_10+0x10)
-#define WO_TRIANGLE_40 (WO_TRIANGLE_20+0x20)
-#define WO_TRIANGLE_80 (WO_TRIANGLE_40+0x40)
-#define WO_SAWTOOTH_04 (WO_TRIANGLE_80+0x80)
-#define WO_SAWTOOTH_08 (WO_SAWTOOTH_04+0x04)
-#define WO_SAWTOOTH_10 (WO_SAWTOOTH_08+0x08)
-#define WO_SAWTOOTH_20 (WO_SAWTOOTH_10+0x10)
-#define WO_SAWTOOTH_40 (WO_SAWTOOTH_20+0x20)
-#define WO_SAWTOOTH_80 (WO_SAWTOOTH_40+0x40)
-#define WO_SQUARES     (WO_SAWTOOTH_80+0x80)
-#define WO_WHITENOISE  (WO_SQUARES+(0x80*0x20))
-#define WO_HIGHPASSES  (WO_WHITENOISE+WHITENOISELEN)
-#define WAVES_SIZE     (WO_HIGHPASSES+((0xfc+0xfc+0x80*0x1f+0x80+3*0x280)*31))
+#include "../hvl_tables.h"
 
-extern const int16 vib_tab[64];
-
-extern const uint16 period_tab[61];
-
-extern const int32 stereopan_left[5];
-extern const int32 stereopan_right[5];
-
-extern int8 waves[WAVES_SIZE];
-extern uint32 panning_left[256], panning_right[256];
-
-void hvl_GenTables( void );
+#endif
